486f5eee02ee883bf3663a47f01faf7960fc6e35
Ric Allinson <capecodehq@yahoo.co.uk>
2015-08-26 13:58:49 -0700

    Bug fix

--- renderer.go
+++ renderer.go
@@ -1,28 +1,28 @@
 package mustache
 
-import(
-    "github.com/hoisie/mustache"
+import (
+	"github.com/hoisie/mustache"
 )
 
 type Mustache struct {
-    cache map[string]*mustache.Template
+	cache map[string]*mustache.Template
 }
 
-func Create() (*Mustache) {
-    this := &Mustache{}
-    this.cache = map[string]*mustache.Template{}
-    return this
+func Create() *Mustache {
+	this := &Mustache{}
+	this.cache = map[string]*mustache.Template{}
+	return this
 }
 
-func (this *Fmustache) Render(f string, o ...interface{}) (string, error) {
-    t, ok := this.cache[f]
-    if ok == false {
-        tmpl, err := mustache.ParseFile(f)
-        if err != nil {
-            return "", err
-        }
-        this.cache[f] = tmpl
-        t = this.cache[f]
-    }
-    return t.Render(o...), nil
+func (this *Mustache) Render(f string, o ...interface{}) (string, error) {
+	t, ok := this.cache[f]
+	if ok == false {
+		tmpl, err := mustache.ParseFile(f)
+		if err != nil {
+			return "", err
+		}
+		this.cache[f] = tmpl
+		t = this.cache[f]
+	}
+	return t.Render(o...), nil
 }
